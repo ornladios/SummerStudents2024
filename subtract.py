fd171fde2211d9023b2ed60ecf43007b16d8545d
Summer Wu <summwu@ad.unc.edu>
2024-07-11 21:36:06 -0400

    Made changes to print out what the code is writing into the output file(file1-file2)

--- subtract.py
+++ subtract.py
@@ -5,12 +5,9 @@ from adios2 import Adios
 from adios2 import FileReader, Stream
 
 if len(sys.argv) < 8:
-
-    print(f"Usage: {sys.argv[0]} bpfile funcname Lname bpfile1 funcname1 Lname1")
-
+    print(f"Usage: {sys.argv[0]} bpfile funcname Lname bpfile1 funcname1 Lname1 output_file")
     sys.exit(1)
 
- 
 fname = sys.argv[1]
 funcname = sys.argv[2]
 Lname = sys.argv[3]
@@ -19,29 +16,25 @@ funcname1 = sys.argv[5]
 Lname1 = sys.argv[6]
 output_file = sys.argv[7]
 
-# some empty lists to store the data
-
+print(f"Reading data from the first file: {fname}")
 funcdata = []
 Ldata = []
-funcdata1 = []
-Ldata1 = []
-
-# Reading data from the first file
-
 with Stream(fname, "r") as f:
     for _ in f.steps():
         funcdata.append(f.read(funcname))
         Ldata.append(f.read(Lname))
+print(f"Finished reading data from the first file: {fname}")
 
-# Reading data from the second file
-
+print(f"Reading data from the second file: {fname1}")
+funcdata1 = []
+Ldata1 = []
 with Stream(fname1, "r") as f:
     for _ in f.steps():
         funcdata1.append(f.read(funcname1))
         Ldata1.append(f.read(Lname1))
+print(f"Finished reading data from the second file: {fname1}")
 
-# Convert lists to numpy arrays
-
+print("Converting lists to numpy arrays")
 funcdata = np.concatenate(funcdata)
 Ldata = np.concatenate(Ldata)
 funcdata1 = np.concatenate(funcdata1)
@@ -50,44 +43,46 @@ L = Ldata.flatten()
 L1 = Ldata1.flatten()
 F = funcdata.flatten()
 F1 = funcdata1.flatten()
+
+# Truncate the longer array to match the shorter one
+min_length = min(len(L), len(L1))
+L = L[:min_length]
+L1 = L1[:min_length]
+
+min_length = min(len(F), len(F1))
+F = F[:min_length]
+F1 = F1[:min_length]
+
 Ldiff = L - L1
 Fdiff = F - F1
+print("Finished converting lists to numpy arrays")
 
+print(f"Writing differences to the output file: {output_file}")
 adios = Adios("adios2.xml")
 ioOut = adios.declare_io("uncompressed error")
 fout = Stream(ioOut, output_file, "w")
 
-with Stream("output_file","w") as s:
-
-        fout.begin_step()
-        fout.write("Fdiff", Fdiff, [len(Fdiff)], [0], [len(Fdiff)])
-        fout.write("Ldiff", Ldiff, [len(Ldiff)], [0], [len(Ldiff)])
-        fout.end_step()
+with Stream("output_file", "w") as s:
+    fout.begin_step()
+    fout.write("Fdiff", Fdiff, [len(Fdiff)], [0], [len(Fdiff)])
+    fout.write("Ldiff", Ldiff, [len(Ldiff)], [0], [len(Ldiff)])
+    fout.end_step()
 
 fout.close()
+print(f"Finished writing differences to the output file: {output_file}")
 
-# for i in range(1):
+# Optionally, you can print out the differences or create a plot
+print("Differences in F:")
+print(Fdiff)
+print("Differences in L:")
+print(Ldiff)
 
-#     N = len(Ldata[i])
-#     n = len(Ldata1[i])
-#     L = Ldata[i][0:N:2, 0:N:2, 0:N:2]
-#     L1 = Ldata1[i][0:n]
-#     print(L.shape, L1.shape)
-#     # align? match
-#     assert len(L) == len(L1)
-#     # make sure the isze is 257 for big and 128 for small
-#     # Ldiff = L -L1 fix
-#     print(f"BIG {N} small {n}")
-
-
-# L = L.flatten()
-# L1 = L1.flatten()
-# Ldiff = L - L1
-# print(Ldiff[-1000:])
-# #
-# rel_diff = np.abs(Ldiff) / (L.max()-L.min())
-# print(np.where(np.reshape(rel_diff, [129,129,129])>0.1))
-# rel_diff2 = np.reshape(rel_diff, [129,129,129])[1:-2,1:-2,1:-2]
-# plt.hist(rel_diff2.flatten())
-# plt.title("Histogram ")
+# If you want to create a plot
+# plt.hist(Fdiff, bins=50, alpha=0.5, label='Fdiff')
+# plt.hist(Ldiff, bins=50, alpha=0.5, label='Ldiff')
+# plt.legend(loc='upper right')
+# plt.title("Histogram of Differences")
+# plt.xlabel("Difference")
+# plt.ylabel("Frequency")
 # plt.show()
+
